9efe47e9e3722ac17ccd05a3d11515005ed1d979
koseoyeon <woogwayeon@gmail.com>
2025-06-12 14:40:24 +0900

    파이선 스레드와 프로세스, Pool

--- 25HC_0612_python_Thread_Process/pool_apply.py
+++ 25HC_0612_python_Thread_Process/pool_apply.py
@@ -7,14 +7,16 @@ def myTesk(n):
     print(f"Completed : {n} // {multiprocessing.current_process().pid}")
     return n
 
-
 def main():
     print("\n\ntesk allocation with map\n\n")
     t0 = time.time()
 
     with Pool(4) as p:
-        print(p.map(myTesk, [4, 3, 2, 1]))
-
+        result = []
+        for i in [4, 3, 2, 1]:
+            result.append(p.apply(myTesk, (i,)))
+        print(result)
+        
     print(f"\n\ntotal time : {time.time() - t0:.2f} sec")
 
 
